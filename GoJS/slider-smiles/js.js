20d9b75a0e10e6d9b7283c36b449b2b2ffe9c91c
Sergey <garbovsky.s@gmail.com>
2015-08-27 17:49:54 +0300

    ///

--- GoJS/slider-smiles/js.js
+++ GoJS/slider-smiles/js.js
@@ -6,13 +6,16 @@ function Slider() {
     var images = document.body.querySelectorAll('img');
 
     var counter = 0;
-
+    rightButton.style.color = '#e3e3e3';
     //Methods
     leftButton.onclick = function () {
         if (counter >= 0 && counter <= 6) {
             images[counter].classList.remove('active');
             images[counter + 3].classList.add('active');
-
+            rightButton.style.color = 'black';
+            if (counter === 6) {
+                leftButton.style.color = '#e3e3e3';
+            }
             counter++
         }
         console.log(counter);
@@ -22,10 +25,17 @@ function Slider() {
         if (counter >= 1 && counter <= 7) {
             images[counter - 1].classList.add('active');
             images[counter + 2].classList.remove('active');
+            if (counter === 5) {
+                leftButton.style.color = 'black';
+            }
 
-
-            counter--;
         }
-        console.log(counter);
-    }
+        if (counter === 0) {
+            rightButton.style.color = '#e3e3e3';
+            console.log(counter);
+        }
+        counter--;
+    };
+
+
 }
