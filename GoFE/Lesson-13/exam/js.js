85abec4be0b58a60cb35a189c134c21b7a9bc6f2
Sergey <garbovsky.s@gmail.com>
2015-08-28 17:22:48 +0300

    ///

--- GoFE/Lesson-13/exam/js.js
+++ GoFE/Lesson-13/exam/js.js
@@ -1,17 +1,43 @@
 function Gunman() {
     //Local variables
     var gunman = document.body.querySelector('.gunman');
-    var linearGunmanCounter = -50;
+    var pathGunmanCounter = -100; //in the end set -100px
+    var bcgGunmanCounter = 0;
 
     //Timers
-    var timerId = setInterval(function () {
-        linearGunmanCounter += 50;
-        gunman.style.left = linearGunmanCounter + 'px';
-        console.log(gunman.style.left);
-    }, 500);
+    var timerId;
+    timerId = setInterval(function () {
+            pathGunmanCounter += 10;
+            gunman.style.right = pathGunmanCounter + 'px';
+
+            switch (bcgGunmanCounter) {
+                case 0:
+                    gunman.style.backgroundPositionX = bcgGunmanCounter + 'px';
+                    bcgGunmanCounter -= 99;
+                    break;
+
+                case -99:
+                    gunman.style.backgroundPositionX = bcgGunmanCounter + 'px';
+                    bcgGunmanCounter -= 99;
+                    break;
+
+                case -198:
+                    gunman.style.backgroundPositionX = bcgGunmanCounter + 'px';
+                    bcgGunmanCounter += 198;
+                    break;
+            }
+
+            console.log(gunman.style.backgroundPositionX);
+            // console.log(gunman.style.right);
+
+        }
+        ,
+        200
+    );
 
     setInterval(function () {
         clearInterval(timerId);
-    }, 5000)
+        gunman.style.backgroundPositionX = "-300px";
+    }, 10000)
 
 }
