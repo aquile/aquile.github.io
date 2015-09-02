a5c53d49953da647f0b94d0fb626a03f1d4889bd
aquile_bernadotte <garbovsky.s@gmail.com>
2015-09-02 22:28:15 +0300

    ..

--- AJAX/weather.js
+++ AJAX/weather.js
@@ -1,66 +1,6 @@
-var cities;
+var cities,
+    objects;
 
-function Weather() {
-    this.findCity = function (name) {
-
-        Ajax.getJSON('https://geocode-maps.yandex.ru/1.x/?format=json&kind=locality&geocode=' + name, function (geoData) {
-            var objects = geoData.response.GeoObjectCollection.featureMember.map(function (fm) {
-
-                var go = fm.GeoObject,
-                    cordinates = go.Point.pos.split(' ');
-                cities = {
-                    name: go.name,
-                    description: go.description,
-                    kind: go.metaDataProperty.GeocoderMetaData.kind,
-                    longitude: cordinates[0],
-                    latitude: cordinates[1]
-                };
-
-                setTimeout(function () {
-                    var cityDivs = document.querySelectorAll('.city');
-
-                    for (var i = 0; i < cityDivs.length; i++) {
-                        cityDivs[i].addEventListener('click', function () {
-                            var urlWeather = 'http://api.openweathermap.org/data/2.5/weather?lat=' + cities.latitude + '&lon=' + cities.longitude;
-
-                            Ajax.getJSON(urlWeather, function (weatherData) {
-                                console.log("______ ");
-                                console.log(weatherData);
-                                document.querySelector('.weather__info').innerHTML = Templates.Weather.render({
-                                    weather: weatherData
-                                });
-                                return weatherData;
-
-                            });
-                        })
-                    }
-                }, 1000);
-
-
-                return cities;
-            });
-            /* .filter(function(o){
-             return o.kind === 'locality';
-             }); //left in list only cities*/
-
-            document.querySelector('.weather__cities').innerHTML = Templates.Cities.render({
-                cities: objects
-            });
-
-
-        });
-
-        //
-        //this.getWeather = function () {
-        //    var urlWeather = 'http://api.openweathermap.org/data/2.5/weather?lat=' + cities.latitude + '&lon=' + cities.longitude;
-        //
-        //    Ajax.getJSON(urlWeather, function (weatherData) {
-        //        console.log("______ ");
-        //        console.log(weatherData);
-        //    });
-        //}
-    }
-}
 var Templates = {
     Cities: Hogan.compile(document.querySelector('.templates__city').innerHTML),
     Weather: Hogan.compile(document.querySelector('.templates__weather').innerHTML)
@@ -79,3 +19,61 @@ var Ajax = {
 
     }
 };
+
+function findWeather() {
+
+    var cityDivs = document.querySelectorAll('.city');
+    console.log(cityDivs[0]);
+
+    for (var i = 0; i < cityDivs.length - 1; i++) {
+        console.log(cityDivs[i]);
+
+        cityDivs[i].addEventListener('click', function () {
+            var urlWeather = 'http://api.openweathermap.org/data/2.5/weather?lat=' + objects[i].longitude + '&lon=' + objects[i].latitude;
+
+            Ajax.getJSON(urlWeather, function (weatherData) {
+                    console.log("______ ");
+                    console.log(weatherData);
+                    document.querySelector('.weather__info').innerHTML = Templates.Weather.render({
+                        weather: weatherData
+                    });
+                    return weatherData;
+                }
+            )
+        });
+    }
+
+}
+
+function Weather() {
+
+
+    this.findCity = function (name) {
+
+        Ajax.getJSON('https://geocode-maps.yandex.ru/1.x/?format=json&kind=locality&geocode=' + name, function (geoData) {
+            objects = geoData.response.GeoObjectCollection.featureMember.map(function (fm) {
+                console.log(geoData);
+                var go = fm.GeoObject,
+                    cordinates = go.Point.pos.split(' ');
+                cities = {
+                    name: go.name,
+                    description: go.description,
+                    kind: go.metaDataProperty.GeocoderMetaData.kind,
+                    longitude: cordinates[0].slice(0, 2),
+                    latitude: cordinates[1].slice(0, 2)
+                };
+
+                return cities;
+            });
+            /*.filter(function(o){
+             return o.kind === 'locality';
+             }); //left in list only cities*/
+
+            document.querySelector('.weather__cities').innerHTML = Templates.Cities.render({
+                cityObj: objects
+            });
+        });
+    };
+
+
+}
