8388cd6538f9ef23f806b595804be33c379219d2
aquile_bernadotte <garbovsky.s@gmail.com>
2015-09-26 20:59:21 +0300

    working version of js.js should be renamed to gunman.js in next version

--- exam/js.js
+++ exam/js.js
@@ -1,6 +1,6 @@
 function Gunman() {
- console.log("ok");
 
+    var self = this;
     //Local variables
     var gunmanDiv = document.body.querySelector('.gunman'),
         fireDiv = document.body.querySelector('.fire'),
@@ -8,9 +8,9 @@ function Gunman() {
         timerGunmanSpan = document.body.querySelector('.timerNodeGunman'),
         rewardSpan = document.body.querySelector('.rewardNode'),
         loseDiv = document.body.querySelector('.lose'),
-        pathGunmanCounter = -100,
-        i = 0, //bcgGunmanCounter
-        timerYou,
+        pathGunmanCounter = 0,
+        i = 0,  //bcg-x GunmanCounter
+        j = 0,  //bcg-x GunmanCounter
         timerGunmanCounter = 1.5,
         rewardCounter = 0;
 
@@ -25,123 +25,191 @@ function Gunman() {
         gunmanKilled = [8, 9, 10],
         youLose = 11;
 
-
-
-    //GunmanTimer(timerGunmanCounter);
-
-    //Reward(rewardCounter);
-
-    //Lose(100);
-
-    var timerStartGameId = setInterval(Go(X, Y), 200);
-
-    setTimeout(function () {
-
-        clearInterval(timerStartGameId);
-        gunmanDiv.style.backgroundPositionX = stay*X + "px";
-
-        //Fire();
-
-    }, 4500);
+    //main timer of appearance
+    var appear = 3600;
 
 
     //Local methods
-    function Go(x, y) {
-        console.log("ok2");
-        pathGunmanCounter += 20;
-        gunmanDiv.style.right = pathGunmanCounter + 'px';
+    this.Go = function () {
+        var timerGoId = setInterval(function () {
+            pathGunmanCounter += 20;
+            gunmanDiv.style.right = pathGunmanCounter + 'px';
 
-        for (;i < i+1; i++) {
-            //implementing step of gunman
-            gunmanDiv.style.backgroundPositionX = run[i]*x+ 'px';
-            //restart step
-            if (i == run.length - 1) i = 0;
+            gunmanDiv.style.backgroundPositionX = run[i] * X + 'px';
+            i++;
+
+            if (i == 2) {
+                i = 0
+            }
+        }, 200);
+
+        self.GunmanTimer(timerGunmanCounter);
+
+        setTimeout(function () {
+            clearInterval(timerGoId);
+            gunmanDiv.style.backgroundPositionX = stay * X + "px";
+
+            self.Ready();
+        }, appear);
+    };
+
+    this.Ready = function () {
+        var border = 15;
+
+        fireDiv.innerHTML = "READY?";
+        fireDiv.style.display = "block";
+
+        var increaseBorderId = setInterval(function () {
+            fireDiv.style.border = border + "px solid red";
+            border -= 1;
+        }, 100);
+
+        setTimeout(function () {
+            clearInterval(increaseBorderId);
+            fireDiv.innerHTML = "FIRE!!!";
+            fireDiv.style.background = "red";
+            fireDiv.style.color = "white";
+            self.Win();
+        }, 1500)
+    };
+
+
+    this.GunmanTimer = function (timer) {
+        var t;
+        timer -= 0.15;
+        timerGunmanCounter = timer;
+        t = timer.toFixed(2) + " s";
+        timerGunmanSpan.innerHTML = t;
+    };
+
+
+    this.Win = function () {
+        var yourTime,
+            past,
+            then;
+
+        past = new Date().getTime();
+        //Set timer for gunman
+
+
+        // Implement Lose function timer
+        var loseID = setTimeout(function () {
+            self.GunmanShot();
+        }, timerGunmanCounter * 1000);  //1500 it's Ready() function Timeout
+
+        //Giving a chance to player))
+        gunmanDiv.addEventListener('click', function () {
+            clearInterval(loseID);
+            then = new Date().getTime();
+            yourTime = ((then - past) / 1000).toFixed(2) + " s";
+
+            timerYouSpan.innerHTML = yourTime;
+            self.Reward(rewardCounter);
+            self.GunmanKilled();
+//
+            //setTimeout(function () {
+            //    pathGunmanCounter = 0;
+            //    i = 0;
+            //    self.Go();
+            //}, 2000)
+        });
+
+    };
+
+
+    this.Reward = function (score) {
+        score += 1000;
+        score = score + " ";
+        //добавить к скоре разницу в милиссекундах между твоим выстрелом и выстрелом ганмена
+        rewardSpan.firstChild.nodeValue = score;
+    };
+
+    this.Lose = function (x) {
+        fireDiv.innerHTML = "LOSER !";
+        fireDiv.style.background = "white";
+        fireDiv.style.color = "red";
+
+
+        var fs = 0,
+            opacity = 0;
+
+        function increaseFS() {
+            fs += 1;
+            opacity += 0.1;
+            loseDiv.style.fontSize = fs + "em";
+            loseDiv.style.opacity = opacity;
         }
-        //switch (bcgGunmanCounter) {
-        //    case 0:
-        //        gunmanDiv.style.backgroundPositionX = bcgGunmanCounter + 'px';
-        //        bcgGunmanCounter -= 96;
-        //        break;
-        //
-        //    case -96:
-        //        gunmanDiv.style.backgroundPositionX = bcgGunmanCounter + 'px';
-        //        bcgGunmanCounter -= 96;
-        //        break;
-        //
-        //    case -192:
-        //        gunmanDiv.style.backgroundPositionX = bcgGunmanCounter + 'px';
-        //        bcgGunmanCounter += 192;
-        //        break;
-        //}
-    }
 
-    //function Fire() {
-    //    var border = 15;
-    //
-    //    fireDiv.innerHTML = "READY?";
-    //    fireDiv.style.display = "block";
-    //
-    //    var increaseBorderId = setInterval(function () {
-    //        fireDiv.style.border = border + "px solid red";
-    //        border -= 1;
-    //    }, 100);
-    //
-    //    setTimeout(function () {
-    //        clearInterval(increaseBorderId);
-    //        fireDiv.innerHTML = "FIRE!!!";
-    //        fireDiv.style.background = "red";
-    //        fireDiv.style.color = "white";
-    //    }, 1500)
-    //}
-//
-//    function Reward(score) {
-//        score += 1000;
-//        score = score + " ";
-//        //добавить к скоре разницу в милиссекундах между твоим выстрелом и выстрелом ганмена
-//        rewardSpan.innerHTML = score;
-//    }
-//
-//    function YouTimer() {
-//
-//    }
-//
-//    function GunmanTimer(timer) {
-//        var t;
-//        timer -= 0.15;
-//        timerGunmanCounter = timer;
-//        t = timer.toFixed(2) + " s";
-//        timerGunmanSpan.innerHTML = t;
-//    }
-//
-//    function Win() {
-//        gunmanDiv.addEventListener('click', function (timer) {
-//            setTimeout(function () {
-//                clearInterval(loseID)
-//            }, timer)
-//        })
-//    }
-//
-//    function Lose(x) {
-//        loseDiv.style.display = "block";
-//
-//        var fs = 0,
-//            opacity = 0;
-//
-//        function increaseFS() {
-//            fs += 1;
-//            opacity += 0.1;
-//            loseDiv.style.fontSize = fs + "em";
-//            loseDiv.style.opacity = opacity;
-//        }
-//
-//        var setId = setInterval(increaseFS, x);
-//
-//        setTimeout(function () {
-//            clearTimeout(setId);
-//            setInterval(function () {
-//                loseDiv.classList.toggle('flashing')
-//            }, x * 5)
-//        }, x * 7)
-//    }
-//
+        var setId = setInterval(increaseFS, x);
+
+        setTimeout(function () {
+            clearTimeout(setId);
+            setInterval(function () {
+                loseDiv.classList.toggle('flashing')
+            }, x * 5)
+        }, x * 7)
+    };
+    this.GoBack = function () {
+        i = youLose;
+        gunmanDiv.style.backgroundPositionX = i * X + "px";
+
+        setTimeout(function () {
+            i = 3;
+            var backId = setInterval(function () {
+                gunmanDiv.style.backgroundPositionX = fireGun[i] * X + "px";
+                i--;
+                if (i == 0) {
+                    //i = 0;
+                    clearInterval(backId);
+                    var intId = setInterval(function () {
+                        pathGunmanCounter += 20;
+                        gunmanDiv.style.right = pathGunmanCounter + 'px';
+
+                        gunmanDiv.style.backgroundPositionX = run[i] * X + 'px';
+                        i++;
+
+                        if (i == 2) {
+                            i = 0
+                        }
+                    }, 200);
+                    setTimeout(function () {
+                        clearInterval(intId)
+                    }, 6000)
+                }
+            }, 250);
+        }, 3000)
+    };
+
+    this.GunmanKilled = function () {
+        fireDiv.innerHTML = "КРАСАУЧЕГ !";
+        i = 0;
+        var killedId = setInterval(function () {
+            gunmanDiv.style.backgroundPositionX = gunmanKilled[i] * X + "px";
+
+            if (i == 2) {
+                clearInterval(killedId);
+            }
+
+            i++;
+        }, 500)
+    };
+
+    this.GunmanShot = function () {
+        //close gunman from the user
+        loseDiv.style.display = "block";
+        loseDiv.style.opacity = "0";
+
+        i = 0;
+        var fireInterval = setInterval(function () {
+            gunmanDiv.style.backgroundPositionX = fireGun[i] * X + "px";
+            i++;
+            if (i == 3) {
+                clearInterval(fireInterval);
+                self.Lose(100);
+                self.GoBack();
+            }
+        }, 500);
+    };
+
+
 }
