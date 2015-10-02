a05c11a73db9924dea35fa0fced9b92e217fe45a
aquile_bernadotte <garbovsky.s@gmail.com>
2015-10-02 23:41:37 +0300

    something wrong

--- wiley-slider/view-slider.js
+++ wiley-slider/view-slider.js
@@ -1,20 +1,25 @@
-function Slider(timeout, transition) {
+function Slider(interval, timeout, transition) {
 
 
     window.onload = setWidths;
     window.onresize = setWidths;
 
+
+
     //Local variables
     var sliderDiv = document.body.querySelector('.slider__images'),
         sliderImgWrp = sliderDiv.querySelector('.slider__images-wrap'),
         sliderImgArr = sliderImgWrp.querySelectorAll('.slider__images img'),
-        paginatorArr = document.body.querySelectorAll('.slider__pagination-item');
+        paginatorArr = document.body.querySelectorAll('.slider__pagination-item'),
+        sliderANodes = sliderImgWrp.querySelectorAll('a'),
+        sliderDivWidth;
+    var slidesCounter = 0,
+        paginatorCounter = 0,
+        slidesCounterMax = sliderImgArr.length; //4
+    var intId;
 
-    //Get width of parent node
-    var sliderDivWidth;
+
     //Set width to images wrapper to put them in line
-
-
     function setWidths() {
         //Set this dimension
         sliderDivWidth = sliderDiv.offsetWidth;
@@ -24,19 +29,14 @@ function Slider(timeout, transition) {
         for (var i = 0; i < sliderImgArr.length; i++) {
             sliderImgArr[i].style.width = sliderDivWidth + "px";
         }
-        console.log('ok');
     }
-
-    var slidesCounter = 0,
-        paginatorCounter = 0,
-        slidesCounterMax = sliderImgArr.length; //4
 
     //add first paginator class
     paginatorArr[0].classList.toggle('active');
 
     function slider() {
-            paginator();
-            styling();
+        paginator();
+        styling();
 
         //if the slider is last one
         if (slidesCounter == slidesCounterMax) {
@@ -45,7 +45,7 @@ function Slider(timeout, transition) {
             slidesCounter = 0;
 
             setTimeout(function () {
-            styling();
+                styling();
             }, 300)
         }
     }
@@ -72,5 +72,72 @@ function Slider(timeout, transition) {
         }
     }
 
-    setInterval(slider, timeout);
+
+    Array.prototype.forEach.call(paginatorArr, function (el, i) {
+        el.addEventListener('click', function () {
+            clearInterval(intId);
+
+            slidesCounter = i - 1;
+            for (var j = 0; j < paginatorArr.length; j++) {
+                paginatorArr[j].classList.remove('active');
+            }
+            paginatorArr[i].classList.add('active');
+            paginatorCounter = i;
+            //console.log(i);
+
+            styling();
+
+            setTimeout(function () {
+                intId = setInterval(slider, interval);
+            }, timeout)
+        })
+
+    });
+
+
+
+    for ( var i = 0; i < sliderANodes.length; i++){
+        sliderANodes[i].addEventListener('mousemove', function(){
+            clearInterval(intId);
+            console.log("in")
+        });
+
+        sliderANodes[i].addEventListener("mouseover", function(){
+            intId = setInterval(slider, interval);
+            console.log("out")
+        })
+    }
+
+    //Array.prototype.forEach.call(sliderImgArr, function (el, i) {
+    //    el.addEventListener("mousemove", function(){
+    //    clearInterval(intId);
+    //        console.log("in")
+    //    });
+    //    el.addEventListener("mouseover", function(){
+    //        intId = setInterval(slider, interval);
+    //        console.log("out")
+    //    })
+    //});
+
+
+    //function fadeIn(el) {
+    //    el.style.opacity = 0;
+    //
+    //    var last = +new Date();
+    //    var tick = function() {
+    //        el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+    //        last = +new Date();
+    //
+    //        if (+el.style.opacity < 1) {
+    //            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    //        }
+    //    };
+    //
+    //    tick();
+    //}
+
+    //fadeIn(el);
+
+    intId = setInterval(slider, interval);
+
 }
