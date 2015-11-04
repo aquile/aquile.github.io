f1fa764be64ce823900c923e7e1034cc1817c424
aquile_bernadotte <garbovsky.s@gmail.com>
2015-11-05 00:13:08 +0200

    keypress binded

--- GL/script/script.js
+++ GL/script/script.js
@@ -1,3 +1,16 @@
-/**
- * Created by aquile_bernadotte on 04.11.15.
- */
+$(document).ready(function () {
+    var inputsArr = $(".form_frame input");
+
+    inputsArr.each(function(index){
+        inputsArr.eq(index).keypress(function () {
+            inputsArr.eq(index + 1).prop("disabled", false);
+            inputsArr.eq(index + 1).focus();
+            setTimeout(function(){
+                inputsArr.eq(index).prop("disabled", true);
+            }, 200);
+        });
+    })
+
+});
+
+
