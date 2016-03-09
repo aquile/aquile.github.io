4bd6b8c92f49ab20b8d1756a4664a6dc53905e41
aquile_bernadotte <garbovsky.s@gmail.com>
2016-03-09 08:52:33 +0200

    changes in public repository according to the fake NDA

--- mannequin-parade/brixton/brixton/includes/boxes/loopBlogQuote.php
+++
@@ -1,1 +0,0 @@
-<?php global $pmc_data; ?>	<div class="entry">		<div class = "meta">			<div class="blogContent">				<div class="blogcontent"><?php the_content(__('','pmc-themes')) ?> </div>			</div>			<div class="blogcontent-title"><?php the_title() ?> </div>		</div>			</div>	
